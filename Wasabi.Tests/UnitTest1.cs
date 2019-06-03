6288451a5e7fe218871c60d9dd3d23d1e3a60f32
DanGould <d@ngould.dev>
2019-06-03 18:05:10 -0400

    add xUnit

---
+++ Wasabi.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace Wasabi.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void PassingTest()
+		{
+			Assert.Equal(4, Add(2, 2));
+		}
+
+		[Fact]
+		public void FailingTest()
+		{
+			Assert.Equal(5, Add(2, 2));
+		}
+
+		int Add(int x, int y)
+		{
+			return x + y;
+		}
+
+	}
+}
