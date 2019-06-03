db7ae413cd0080c35e57e36ab63632c1d566bea0
DanGould <dan@dannygould.com>
2019-06-03 12:42:13 -0400

    initial commit

---
+++ Wasabi/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Wasabi
+{
+	public partial class MainPage : ContentPage
+	{
+		string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			if (File.Exists(_fileName))
+			{
+				editor.Text = File.ReadAllText(_fileName);
+			}
+		}
+
+		void OnSaveButtonClicked(object sender, EventArgs e)
+		{
+			File.WriteAllText(_fileName, editor.Text);
+		}
+
+		void OnDeleteButtonClicked(object sender, EventArgs e)
+		{
+			if (File.Exists(_fileName))
+			{
+				File.Delete(_fileName);
+			}
+			editor.Text = string.Empty;
+		}
+	}
+}
