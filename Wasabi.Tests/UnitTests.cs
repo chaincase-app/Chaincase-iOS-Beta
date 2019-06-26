d7754ec0258bd759d64484df6078509647c6e84e
DanGould <d@ngould.dev>
2019-06-26 12:18:43 -0400

    unit test with settings

---
+++ Wasabi.Tests/UnitTests.cs
@@ -0,0 +1,29 @@
+using NBitcoin;
+using Wasabi.Controllers;
+using Xunit;
+
+namespace Wasabi.Tests
+{
+	public class UnitTests
+	{
+		[Fact]
+		public void AssertGeneratedWalletCredentialsVerifyWithPassphrase()
+		{
+			string passphrase = "passphrase";
+			Mnemonic mnemonic = GenerateWalletController.GenerateMnemonicAsync(passphrase).Result;
+
+			var isVerified = GenerateWalletController.VerifyWalletCredentials(mnemonic.ToString(),  passphrase);
+			Assert.True(isVerified);
+		}
+
+		[Fact]
+		public void AssertGeneratedWalletCredentialsVerifyWithoutPassphrase()
+		{
+			string passphrase = ""; // cannot be null
+			Mnemonic mnemonic = GenerateWalletController.GenerateMnemonicAsync(passphrase).Result;
+
+			var isVerified = GenerateWalletController.VerifyWalletCredentials(mnemonic.ToString(), passphrase);
+			Assert.True(isVerified);
+		}
+	}
+}
