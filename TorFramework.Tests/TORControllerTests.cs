d04ff9970e65d44b86bbe42a1a1db349b8388cd9
DanGould <d@ngould.dev>
2019-11-26 21:53:16 -0500

    setUp TORController for tests

--- TorFramework.Tests/TORControllerTests.cs
+++ TorFramework.Tests/TORControllerTests.cs
@@ -10,6 +10,7 @@ namespace TorFramework.Tests
 	public class TORControllerTests
 	{
 		private TORConfiguration configuration;
+        private TORController controller;
 
 		private void SetUp()
 		{
@@ -23,25 +24,94 @@ namespace TorFramework.Tests
 			TORThread thread = new TORThread(configuration);
 			thread.Start();
 			NSRunLoop.Main.RunUntil(NSDate.FromTimeIntervalSinceNow(0.5f));
-		}
 
+            controller = new TORController(configuration.ControlSocket);
+        }
+
+        //- (void) testCookieAuthenticationFailure
+        //        {
+        //            XCTestExpectation *expectation = [self expectationWithDescription:@"authenticate callback"];
+        //            [self.controller authenticateWithData:[@"invalid" dataUsingEncoding:NSUTF8StringEncoding]
+        //            completion:^(BOOL success, NSError* error) {
+        //             XCTAssertFalse(success);
+        //             XCTAssertEqualObjects(error.domain, TORControllerErrorDomain);
+        //            XCTAssertNotEqual(error.code, 250);
+        //            XCTAssertGreaterThan(error.localizedDescription, @"Authentication failed: Wrong length on authentication cookie.");
+        //            [expectation fulfill];
+        //    }];
+    
+        //    [self waitForExpectationsWithTimeout:1.0f handler:nil];
+
+        //   }
+
+        [Test]
+        public void TestCookieAutheniticationFailure()
+        {
+
+            Assert.True(false);
+        }
+
+ //- (void) testCookieAuthenticationSuccess
+ //   {
+ //       XCTestExpectation *expectation = [self expectationWithDescription:@"authenticate callback"];
+
+ //       NSURL *cookieURL = [[[[self class] configuration] dataDirectory] URLByAppendingPathComponent:@"control_auth_cookie"];
+ //   NSData* cookie = [NSData dataWithContentsOfURL: cookieURL];
+ //   [self.controller authenticateWithData:cookie completion:^(BOOL success, NSError *error) {
+ //       XCTAssertTrue(success);
+ //       XCTAssertNil(error);
+ //       [expectation fulfill];
+ //   }];
+    
+ //   [self waitForExpectationsWithTimeout:1.0f handler:nil];
+//}
+
+        [Test]
+        public void TestCookieAuthentiicationSuccess()
+        {
+            Assert.True(false);
+        }
+
+
+
+//- (void) testSessionConfiguration
+//        {
+//            XCTestExpectation *expectation = [self expectationWithDescription:@"tor callback"];
+
+//            TORController *controller = self.controller;
+
+//    void (^test)(void) = ^{
+//        [controller getSessionConfiguration:^(NSURLSessionConfiguration* configuration) {
+//            NSURLSession* session = [NSURLSession sessionWithConfiguration: configuration];
+//        [[session dataTaskWithURL:[NSURL URLWithString:@"https://facebookcorewwwi.onion/"] completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
+//                XCTAssertEqual([(NSHTTPURLResponse *)response statusCode], 200);
+//        XCTAssertNil(error);
+//        [expectation fulfill];
+//            }] resume];
+//        }];
+//    };
+    
+//    NSURL* cookieURL = [[[[self class] configuration] dataDirectory] URLByAppendingPathComponent:@"control_auth_cookie"];
+//    NSData* cookie = [NSData dataWithContentsOfURL: cookieURL];
+//    [controller authenticateWithData:cookie completion:^(BOOL success, NSError *error) {
+//        if (!success)
+//            return;
+
+
+//        [controller addObserverForCircuitEstablished:^(BOOL established) {
+//            if (!established)
+//                return;
+
+
+//        test();
+//        }];
+//    }];
+    
+  
 		[Test]
-		public void Pass()
+		public void TestSessionConfiguration()
 		{
 			SetUp();
-			Assert.True(true);
-		}
-
-		[Test]
-		public void Fail()
-		{
-			Assert.False(true);
-		}
-
-		[Test]
-		[Ignore("another time")]
-		public void Ignore()
-		{
 			Assert.True(false);
 		}
 	}
