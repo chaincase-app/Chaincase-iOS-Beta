7f59d072a4db1d60027037a542068690c592562c
DanGould <d@ngould.dev>
2019-11-27 20:52:32 -0500

    frame test2

--- TorFramework.Tests/TORControllerTests.cs
+++ TorFramework.Tests/TORControllerTests.cs
@@ -3,6 +3,7 @@ using Foundation;
 using System;
 using NUnit.Framework;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace TorFramework.Tests
 {
@@ -12,7 +13,8 @@ namespace TorFramework.Tests
 		private TORConfiguration configuration;
         private TORController controller;
 
-		private void SetUp()
+        [TestFixtureSetUp]
+		protected void SetUp()
 		{
 			configuration = new TORConfiguration();
 			configuration.CookieAuthentication = new Foundation.NSNumber(true); //@YES
@@ -44,74 +46,83 @@ namespace TorFramework.Tests
 
         //   }
 
-        [Test]
-        public void TestCookieAutheniticationFailure()
-        {
+        //[Test]
+        //public void TestCookieAutheniticationFailure()
+        //{
+        //    //Action<bool, NSError> callback =
+        //    var (success, error) = controller.AuthenticateWithDataAsync(new NSString("invalid").Encode(NSStringEncoding.UTF8)).Result;
+        //    Assert.False(success);
+        //    Assert.True(string.Equals(error.Domain, Constants.TORControllerErrorDomain.ToString()));
+        //    Assert.True(error.Code != new nint(250));
+        //}
 
-            Assert.True(false);
-        }
+         //- (void) testCookieAuthenticationSuccess
+         //   {
+         //       XCTestExpectation *expectation = [self expectationWithDescription:@"authenticate callback"];
 
- //- (void) testCookieAuthenticationSuccess
- //   {
- //       XCTestExpectation *expectation = [self expectationWithDescription:@"authenticate callback"];
-
- //       NSURL *cookieURL = [[[[self class] configuration] dataDirectory] URLByAppendingPathComponent:@"control_auth_cookie"];
- //   NSData* cookie = [NSData dataWithContentsOfURL: cookieURL];
- //   [self.controller authenticateWithData:cookie completion:^(BOOL success, NSError *error) {
- //       XCTAssertTrue(success);
- //       XCTAssertNil(error);
- //       [expectation fulfill];
- //   }];
+         //       NSURL *cookieURL = [[[[self class] configuration] dataDirectory] URLByAppendingPathComponent:@"control_auth_cookie"];
+         //   NSData* cookie = [NSData dataWithContentsOfURL: cookieURL];
+         //   [self.controller authenticateWithData:cookie completion:^(BOOL success, NSError *error) {
+         //       XCTAssertTrue(success);
+         //       XCTAssertNil(error);
+         //       [expectation fulfill];
+         //   }];
     
- //   [self waitForExpectationsWithTimeout:1.0f handler:nil];
-//}
+         //   [self waitForExpectationsWithTimeout:1.0f handler:nil];
+        //}
 
         [Test]
         public void TestCookieAuthentiicationSuccess()
         {
-            Assert.True(false);
+            NSUrl cookieUrl = configuration.DataDirectory.Append("control_auth_cookie", false);
+            NSData cookie = NSData.FromUrl(cookieUrl);
+            var (success, error) = controller.AuthenticateWithDataAsync(cookie).Result;
+            Assert.True(success);
+            Assert.True(error is null);
+            
+            //file:///Users/dan/Library/Developer/CoreSimulator/Devices/F56F4323-8547-4BF4-A437-973105124CFF/data/Containers/Data/Application/F9538CF0-5B57-4DCA-8733-5CF7345F6832/tmp.control_auth_cookie/}
+            //file:///Users/dan/Library/Developer/CoreSimulator/Devices/F56F4323-8547-4BF4-A437-973105124CFF/data/Containers/Data/Application/A29FD3D0-5104-4F46-BF3B-C7FEE6D5DE1F/tmp.control_auth_cookie/}
         }
 
 
 
-//- (void) testSessionConfiguration
-//        {
-//            XCTestExpectation *expectation = [self expectationWithDescription:@"tor callback"];
+        //- (void) testSessionConfiguration
+        //        {
+        //            XCTestExpectation *expectation = [self expectationWithDescription:@"tor callback"];
 
-//            TORController *controller = self.controller;
+            //            TORController *controller = self.controller;
 
-//    void (^test)(void) = ^{
-//        [controller getSessionConfiguration:^(NSURLSessionConfiguration* configuration) {
-//            NSURLSession* session = [NSURLSession sessionWithConfiguration: configuration];
-//        [[session dataTaskWithURL:[NSURL URLWithString:@"https://facebookcorewwwi.onion/"] completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
-//                XCTAssertEqual([(NSHTTPURLResponse *)response statusCode], 200);
-//        XCTAssertNil(error);
-//        [expectation fulfill];
-//            }] resume];
-//        }];
-//    };
-    
-//    NSURL* cookieURL = [[[[self class] configuration] dataDirectory] URLByAppendingPathComponent:@"control_auth_cookie"];
-//    NSData* cookie = [NSData dataWithContentsOfURL: cookieURL];
-//    [controller authenticateWithData:cookie completion:^(BOOL success, NSError *error) {
-//        if (!success)
-//            return;
+            //    void (^test)(void) = ^{
+            //        [controller getSessionConfiguration:^(NSURLSessionConfiguration* configuration) {
+            //            NSURLSession* session = [NSURLSession sessionWithConfiguration: configuration];
+            //        [[session dataTaskWithURL:[NSURL URLWithString:@"https://facebookcorewwwi.onion/"] completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
+            //                XCTAssertEqual([(NSHTTPURLResponse *)response statusCode], 200);
+            //        XCTAssertNil(error);
+            //        [expectation fulfill];
+            //            }] resume];
+            //        }];
+            //    };
 
-
-//        [controller addObserverForCircuitEstablished:^(BOOL established) {
-//            if (!established)
-//                return;
+            //    NSURL* cookieURL = [[[[self class] configuration] dataDirectory] URLByAppendingPathComponent:@"control_auth_cookie"];
+            //    NSData* cookie = [NSData dataWithContentsOfURL: cookieURL];
+            //    [controller authenticateWithData:cookie completion:^(BOOL success, NSError *error) {
+            //        if (!success)
+            //            return;
 
 
-//        test();
-//        }];
-//    }];
-    
-  
-		[Test]
+            //        [controller addObserverForCircuitEstablished:^(BOOL established) {
+            //            if (!established)
+            //                return;
+
+
+            //        test();
+            //        }];
+            //    }];
+
+
+        [Test]
 		public void TestSessionConfiguration()
 		{
-			SetUp();
 			Assert.True(false);
 		}
 	}
