88823a712722a7ceed09aca1f9d671a37f0920d2
DanGould <d@ngould.dev>
2019-12-14 22:21:57 -0500

    add coinlist to main

--- Chaincase/ViewModels/MainViewModel.cs
+++ Chaincase/ViewModels/MainViewModel.cs
@@ -7,50 +7,69 @@ using System.Windows.Input;
 using NBitcoin;
 using ReactiveUI;
 using WalletWasabi.Models;
+using WalletWasabi.Logging;
 using Chaincase.Controllers;
 using Chaincase.Navigation;
 using Xamarin.Forms;
+using Splat;
 
 namespace Chaincase.ViewModels
 {
 	public class MainViewModel : ViewModelBase
 	{
 		private CompositeDisposable Disposables { get; set; }
+        private CoinListViewModel _coinList;
+        public CoinListViewModel CoinList
+        {
+            get => _coinList;
+            set => this.RaiseAndSetIfChanged(ref _coinList, value);
+        }
 
-		private String _balance;
+        private String _balance;
 		public String Balance
 		{
 			get => _balance;
 			set => this.RaiseAndSetIfChanged(ref _balance, value);
 		}
 
-		public ReactiveCommand<Unit, Unit> NavReceiveCommand;
+        public ReactiveCommand<Unit, Unit> NavReceiveCommand;
 		public ReactiveCommand<Unit, Unit> NavSendCommand;
 
-		public MainViewModel(IScreen hostScreen) : base(hostScreen)
-		{
-			SetBalance();
+        public ReactiveCommand<Unit, Unit> CoinJoin { get; private set; }
+        readonly ObservableAsPropertyHelper<bool> _isJoining;
+        public bool IsJoining { get { return _isJoining.Value; } }
 
-			if (Disposables != null)
-			{
-				throw new Exception("Wallet opened before it was closed.");
-			}
+        public Label Deq;
 
-			Disposables = new CompositeDisposable();
+        public MainViewModel(IScreen hostScreen) : base(hostScreen)
+        {
+            SetBalance();
 
-			NavReceiveCommand = ReactiveCommand.CreateFromObservable(() =>
-			{
-				HostScreen.Router.Navigate.Execute(new ReceiveViewModel(hostScreen)).Subscribe();
-				return Observable.Return(Unit.Default);
-			});
+            if (Disposables != null)
+            {
+                throw new Exception("Wallet opened before it was closed.");
+            }
 
-			NavSendCommand = ReactiveCommand.CreateFromObservable(() =>
-			{
-				HostScreen.Router.Navigate.Execute(new SendViewModel(hostScreen)).Subscribe();
-				return Observable.Return(Unit.Default);
-			});
+            Disposables = new CompositeDisposable();
+            CoinList = new CoinListViewModel(hostScreen);
 
-			Observable.FromEventPattern(Global.WalletService.Coins, nameof(Global.WalletService.Coins.CollectionChanged))
+            NavReceiveCommand = ReactiveCommand.CreateFromObservable(() =>
+            {
+                HostScreen.Router.Navigate.Execute(new ReceiveViewModel(hostScreen)).Subscribe();
+                return Observable.Return(Unit.Default);
+            });
+
+            NavSendCommand = ReactiveCommand.CreateFromObservable(() =>
+            {
+                HostScreen.Router.Navigate.Execute(new SendViewModel(hostScreen)).Subscribe();
+                return Observable.Return(Unit.Default);
+            });
+
+            CoinJoin = ReactiveCommand.CreateFromObservable(CoinJoinImpl);
+            CoinJoin.IsExecuting.ToProperty(this, x => x.IsJoining, out _isJoining);
+            CoinJoin.ThrownExceptions.Subscribe(ex => Logger.LogError<MainViewModel>(ex));
+
+            Observable.FromEventPattern(Global.WalletService.Coins, nameof(Global.WalletService.Coins.CollectionChanged))
 				.Merge(Observable.FromEventPattern(Global.WalletService, nameof(Global.WalletService.CoinSpentOrSpenderConfirmed)))
 				.ObserveOn(RxApp.MainThreadScheduler)
 				.Subscribe(o => SetBalance())
@@ -61,5 +80,15 @@ namespace Chaincase.ViewModels
 		{
 			Balance = WalletController.GetBalance().ToString();
 		}
-	}
+
+        public IObservable<Unit> CoinJoinImpl()
+        {
+            Deq.Text = "dequeue?";
+            return Observable.Start(() =>
+            {
+                Task.Delay(500);
+                Deq.Text = "";
+            });
+        }
+    }
 }
--- Chaincase/Views/MainPage.xaml.cs
+++ Chaincase/Views/MainPage.xaml.cs
@@ -12,31 +12,40 @@ namespace Chaincase.Views
 	public partial class MainPage : ReactiveContentPage<MainViewModel>
 	{
 
-        private CompositeDisposable Disposables { get; set; }
-
         public MainPage()
         {
             InitializeComponent();
 
-            this.WhenActivated(Disposables =>
+            this.WhenActivated(d =>
             {
                 this.OneWayBind(ViewModel,
                     vm => vm.Balance,
                     v => v.Balance.Text)
-                    .DisposeWith(Disposables);
+                    .DisposeWith(d);
+                this.OneWayBind(ViewModel,
+                    vm => vm.CoinList,
+                    v => v.CoinList.ViewModel)
+                    .DisposeWith(d);
                 this.BindCommand(ViewModel,
                     vm => vm.NavSendCommand,
                     v => v.NavSendCommand)
-                    .DisposeWith(Disposables);
+                    .DisposeWith(d);
                 this.BindCommand(ViewModel,
                     vm => vm.NavReceiveCommand,
                     v => v.NavReceiveCommand)
-                    .DisposeWith(Disposables);
+                    .DisposeWith(d);
+                this.Bind(ViewModel,
+                    vm => vm.Deq,
+                    v => v.Deq)
+                    .DisposeWith(d);
             });
         }
 
         private void ShowCoinJoinStatus(object sender, EventArgs args)
         {
+            GoPrivateButton.Text = "Go";
+            GoPrivateButton.Clicked -= ShowCoinJoinStatus;
+
             var unitLabel = new Label { Text = "CoinJoin unit: 0.01 BTC" };
             var coinJoinSizeLabel = new Label { Text = "CoinJoin size: 50+ persons" };
             var feeLabel = new Label { Text = "Coordination Fee: 0.5%" };
@@ -45,8 +54,9 @@ namespace Chaincase.Views
             Stackk.Children.Add(feeLabel);
 
             this.BindCommand(ViewModel,
-                vm => vm.CoinJoinCommand,
-                v => v.GoPrivateButton);
+                vm => vm.CoinJoin,
+                v => v.GoPrivateButton,
+                nameof(GoPrivateButton.Clicked));
         }
     }
 }
