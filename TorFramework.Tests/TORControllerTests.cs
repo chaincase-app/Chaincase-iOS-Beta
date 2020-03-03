9f8fe88e1e6b036ef037af2ce7a1cf2cc7c7bd99
DanGould <d@ngould.dev>
2020-03-03 18:56:00 -0500

    remove NUnit

--- TorFramework.Tests/TORControllerTests.cs
+++ TorFramework.Tests/TORControllerTests.cs
@@ -1,30 +1,22 @@
-﻿
 using Foundation;
 using System;
 using NUnit.Framework;
 using System.Runtime.InteropServices;
 using System.Threading;
 using NUnitLite;
+using Xunit;
 using TorFramework;
+using System.Threading.Tasks;
+using ObjCRuntime;
+using Foundation;
 using System.IO;
 using System.Linq;
-using ObjCRuntime;
-using System.Threading.Tasks;
 using System.Threading;
 
 namespace TorFramework.Tests
 {
-
-    interface ITORControllerTests
+    public class TORControllerFixture
     {
-        TORController Controller { get; set; }// @property (nonatomic, strong) TORController *controller;
-        NSData Cookie { get; } // @property (readonly) NSData *cookie;
-    }
-
-
-	[TestFixture]
-	public class TORControllerTests : ITORControllerTests
-	{
         public TORConfiguration Configuration
         {
             get
@@ -58,23 +50,45 @@ namespace TorFramework.Tests
             }
         }
 
-        [TestFixtureSetUp]
-		protected void SetUpOnce()
-		{
-            TORThread thread = new TORThread(Configuration);
-			thread.Start();
+        private TORThread thread;
 
-			NSRunLoop.Main.RunUntil(NSDate.FromTimeIntervalSinceNow(0.5f));
-        }
-
-        public TORController Controller { get; set; }
-        [SetUp]
-        public void SetUp()
+        public TORControllerFixture()
         {
-            Controller = new TORController(Configuration.ControlSocket);
+            // TORThread won't stop til the process does
+            if (TORThread.ActiveThread is null)
+            {
+                thread = new TORThread(Configuration);
+                thread.Start();
+            }
+
+            NSRunLoop.Main.RunUntil(NSDate.FromTimeIntervalSinceNow(0.5f));
         }
 
-        [Test]
+        static string NSHomeDirectory() => Directory.GetParent(NSFileManager.DefaultManager.GetUrls(NSSearchPathDirectory.LibraryDirectory, NSSearchPathDomain.User).First().Path).FullName;
+    }
+
+    [CollectionDefinition("TORControllerCollection", DisableParallelization = true)]
+    public class TORControllerCollection : ICollectionFixture<TORControllerFixture>
+    {
+        // No code, just definition
+    }
+
+    [Collection("TORControllerCollection")]
+    public class ControllerTests : IClassFixture<TORControllerFixture>
+    {
+        TORControllerFixture fixture;
+        public TORController Controller;
+        public NSData Cookie;
+
+
+        public ControllerTests(TORControllerFixture fixture)
+        {
+            this.fixture = fixture;
+            this.Controller = new TORController(fixture.Configuration.ControlSocket);
+            this.Cookie = NSData.FromUrl(fixture.Configuration.DataDirectory.Append("control_auth_cookie", false));
+        }
+
+        [Fact]
         public async Task TestCookieAutheniticationFailure()
         {
             var (success, error) = await Controller.AuthenticateWithDataAsync(new NSString("invalid").Encode(NSStringEncoding.UTF8));
@@ -83,132 +97,90 @@ namespace TorFramework.Tests
             Assert.True(error.Code != new nint(250));
         }
 
-        [Test]
+        [Fact]
         public async Task TestCookieAuthenticationSuccess()
         {
-            NSUrl cookieUrl = Configuration.DataDirectory.Append("control_auth_cookie", false);
-            NSData cookie = NSData.FromUrl(cookieUrl);
-            var (success, error) = await Controller.AuthenticateWithDataAsync(cookie);
+            var (success, error) = await Controller.AuthenticateWithDataAsync(Cookie);
             Assert.True(success);
             Assert.Null(error);
         }
 
+        // hack This, and all, timeouts will just succeed after 120 seconds. If we hang, it's really a failure.
+        [Fact(Timeout = 120 * 1000)]
+        public void TestSessionConfiguration()
+        {
+            EventWaitHandle ewh = new EventWaitHandle(false, EventResetMode.AutoReset);
 
-        //[Test]
-        //public async Task TestSessionConfiguration()
-        //{
-        //    await Exec(async () =>
-        //    {
-        //        NSUrlSessionConfiguration configuration = await Controller.GetSessionConfigurationAsync();
-        //        NSUrlSession session = NSUrlSession.FromConfiguration(configuration);
-        //        session.CreateDataTask(NSUrl.FromString("https://facebookcorewwwi.onion/"), (NSData data, NSUrlResponse response, NSError error) =>
-        //        {
-        //            Assert.Equals(((NSHttpUrlResponse)response).StatusCode, 200);
-        //            Assert.True(error is null);
-        //        }).Resume();
-        //    });
-        //}
+            Exec(() =>
+            {
+                Controller.GetSessionConfiguration((NSUrlSessionConfiguration configuration) =>
+                {
+                    NSUrlSession session = NSUrlSession.FromConfiguration(configuration);
+                    session.CreateDataTask(NSUrl.FromString("https://facebookcorewwwi.onion/"), (NSData data, NSUrlResponse response, NSError error) =>
+                    {
+                        Assert.Equal(((NSHttpUrlResponse)response).StatusCode, 200);
+                        Assert.True(error is null);
+                        ewh.Set();
+                    }).Resume();
+                });
+            });
 
-        //[Test]
-        //public void TestSessionConfiguration()
-        //{
-        //    EventWaitHandle done = new EventWaitHandle(false, EventResetMode.AutoReset);
-        //    NSHttpUrlResponse res = null;
-        //    NSError err = null;
-        //    Exec(delegate()
-        //    {
-        //    //NSUrlSessionConfiguration configuration = Controller.GetSessionConfigurationAsync().Result;
-        //    NSUrlSessionConfiguration config = NSUrlSessionConfiguration.DefaultSessionConfiguration;
-        //    var keys = new NSObject[]
-        //    {
-        //        NSObject.FromObject("kCFProxyTypeKey"),
-        //        NSObject.FromObject("kCFStreamPropertySOCKSProxyHost"),
-        //        NSObject.FromObject("kCFStreamPropertySOCKSProxyPort")
-        //    };
-        //    var vals = new NSObject[]
-        //    {
-        //        NSObject.FromObject("kCFProxyTypeSOCKS"),
-        //        new NSString("localhost"),
-        //        NSNumber.FromInt32(9050)
-        //    };
-        //    config.ConnectionProxyDictionary = new NSDictionary<NSObject, NSObject>(keys, vals);
-        //    NSUrlSession session = NSUrlSession.FromConfiguration(config);
-        //        //session.CreateDataTask(NSUrl.FromString("https://check.torproject.org/"), (NSData data, NSUrlResponse response, NSError error) =>
-        //TODO Decode gzip from check.torproject.org to see if running TOR or not
-        //        session.CreateDataTask(NSUrl.FromString("https://www.facebookcorewwwi.onion"), (NSData data, NSUrlResponse response, NSError error) =>
-        //        {
-        //            res = (NSHttpUrlResponse)response;
-        //            err = error;
-        //            done.Set();
-        //        }).Resume();
-        //    });
-        //    done.WaitOne();
-        //    Assert.True(res.StatusCode == 200);
-        //    Assert.True(err is null);
-        //}
+            ewh.WaitOne();
+        }
 
-        //- (void) testGetAndCloseCircuits
-        //        {
-        //            XCTestExpectation *expectation = [self expectationWithDescription:@"resolution callback"];
+        [Fact(Timeout = 120 * 1000)]
+        public void TestGetAndCloseCircuits()
+        {
+            EventWaitHandle ewh = new EventWaitHandle(false, EventResetMode.AutoReset);
 
-        //            [self exec:^{
-        //        [self.controller getCircuits:^(NSArray<TORCircuit*>* _Nonnull circuits) {
-        //            NSLog(@"circuits=%@", circuits);
+            Exec(() =>
+            {
+                Controller.GetCircuits((NSArray<TORCircuit> circuits) =>
+                {
+                    Console.WriteLine("circuits={0}", circuits);
 
-        //            for (TORCircuit* circuit in circuits)
-        //            {
-        //                for (TORNode* node in circuit.nodes) {
-        //                    XCTAssert(node.fingerprint.length > 0, @"A circuit should have a fingerprint.");
-        //        XCTAssert(node.ipv4Address.length > 0 || node.ipv6Address.length > 0, @"A circuit should have an IPv4 or IPv6 address.");
-        //    }
-        //}
+                    foreach (TORCircuit circuit in circuits)
+                    {
+                        foreach (TORNode node in circuit.Nodes)
+                        {
+                            Assert.True(node.Fingerprint.Length > 0, "A circuit should have a fingerprint.");
+                            Assert.True(node.Ipv4Address.Length > 0 || node.Ipv6Address.Length > 0, "A circuit should have an IPv4 or IPv6 address.");
+                        }
+                    }
 
-        //[self.controller closeCircuits:circuits completion:^(BOOL success) {
+                    Controller.CloseCircuits(circuits.ToArray(), (bool success) =>
+                    {
+                        Assert.True(success, "Circuits were closed successfully");
 
-        //    XCTAssertTrue(success, @"Circuits were closed successfully.");
+                        ewh.Set();
+                    });
+                });
+            });
 
-        //                [expectation fulfill];
-        //            }];
-        //        }];
-        //    }];
+            ewh.WaitOne();
+        }
 
-        //    [self waitForExpectationsWithTimeout:120 handler:nil];
-        //}
-        //[Test]
-        //public async void TestGetAndCloseCircuits()
-        //{
-        //    var autoEvent = new AutoResetEvent(false); // initialize to false
-        //    TORController controller = this.Controller;
+        [Fact(Timeout = 120 * 1000)]
+        public void TestReset()
+        {
+            EventWaitHandle ewh = new EventWaitHandle(false, EventResetMode.AutoReset);
 
-        //    var (success, error) = await controller.AuthenticateWithDataAsync(Cookie);
-        //    Assert.True(success);
-        //    Assert.Null(error);
+            Exec(() =>
+            {
+                Controller.ResetConnection((bool success) =>
+                {
+                    Console.WriteLine("success={0}", success);
 
-        //    var j = controller.AddObserverForCircuitEstablished(async (established) => await ToEstablish(established, autoEvent));
-        //    autoEvent.WaitOne(); // wait until event set
-        //}
+                    Assert.True(success, "Reset should work correctly");
 
-        //public async Task ToEstablish(bool established, AutoResetEvent autoEvent)
-        //{
-        //    //SynchronizationContext.SetSynchronizationContext(new NUnit.Framework.AsyncSynchronizationContext);
-        //    if (!established)
-        //    {
-        //        return;
-        //    }
-        //    NSArray<TORCircuit> circuits = await Controller.GetCircuitsAsync();
-        //    Console.WriteLine("circuits=" + circuits);
-        //    foreach (var circuit in circuits)
-        //    {
-        //        foreach (var node in circuit.Nodes)
-        //        {
-        //            Assert.True(node.Fingerprint.Length > 0, "A circuit should have a fingerprint.");
-        //            Assert.True(node.Ipv4Address.Length > 0 || node.Ipv6Address.Length > 0, "A circuit should have an IPv4 or IPv6 address.");
-        //            autoEvent.Set(); // event set
-        //        }
-        //    }
-        //}
+                    ewh.Set();
+                });
+            });
 
-        public NSData Cookie => NSData.FromUrl(Configuration.DataDirectory.Append("control_auth_cookie", false));
+            ewh.WaitOne();
+        }
+
+        public delegate void Callback();
 
         public void Exec(Callback callback)
         {
@@ -218,6 +190,7 @@ namespace TorFramework.Tests
             Assert.True(success);
             Assert.Null(error);
 
+            // This can't be async'd
             controller.AddObserverForCircuitEstablished((bool established) =>
             {
                 if (!established)
@@ -227,9 +200,5 @@ namespace TorFramework.Tests
                 callback();
             });
         }
-
-        public delegate void Callback();
-
-        static string NSHomeDirectory() => Directory.GetParent(NSFileManager.DefaultManager.GetUrls(NSSearchPathDirectory.LibraryDirectory, NSSearchPathDomain.User).First().Path).FullName;
     }
 }
