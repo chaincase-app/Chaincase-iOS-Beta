14de12be692d92a602e5dad51f10abfc75e50d36
DanGould <d@ngould.dev>
2020-04-20 19:45:43 -0400

    decorate CoinJoinPage labels

--- Chaincase/Views/CoinJoinPage.xaml.cs
+++ Chaincase/Views/CoinJoinPage.xaml.cs
@@ -4,15 +4,21 @@ using ReactiveUI.XamForms;
 using Chaincase.ViewModels;
 using System;
 using Chaincase.Converters;
+using Chaincase.Models;
 
 namespace Chaincase.Views
 {
     public partial class CoinJoinPage : ReactiveContentPage<CoinJoinViewModel>
 	{
+        private TimeSpanStringConverter _timeSpanStringConverter;
+        private PhaseStringConverter _phaseStringConverter;
 
         public CoinJoinPage()
         {
             InitializeComponent();
+
+            _timeSpanStringConverter = new TimeSpanStringConverter();
+            _phaseStringConverter = new PhaseStringConverter();
 
             this.WhenActivated(d =>
             {
@@ -23,18 +29,30 @@ namespace Chaincase.Views
                 this.OneWayBind(ViewModel,
                     vm => vm.RoundPhaseState,
                     v => v.PhaseLabel.Text,
-                    vmToViewConverterOverride: new PhaseStringConverter())
+                    FormatPhaseLabel)
                     .DisposeWith(d);
                 this.OneWayBind(ViewModel,
                     vm => vm.TimeLeftTillRoundTimeout,
                     v => v.TimeLeftLabel.Text,
-                    vmToViewConverterOverride: new TimeSpanStringConverter())
+                    FormatTimeLeftLabel)
                     .DisposeWith(d);
                 this.BindCommand(ViewModel,
                     vm => vm.PromptCommand,
                     v => v.ConfirmButton)
                     .DisposeWith(d);
             });
+        }
+
+        private string FormatTimeLeftLabel(TimeSpan t)
+        {
+            _timeSpanStringConverter.TryConvert(t, typeof(string), null, out var timeString);
+            return "Registration ends in " + timeString;
+        }
+
+        private string FormatPhaseLabel(RoundPhaseState rps)
+        {
+            _phaseStringConverter.TryConvert(rps, typeof(string), null, out var phaseString);
+            return "Phase: " + phaseString;
         }
     }
 }
