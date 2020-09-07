613cb971cf4f6a4d08a9a39a87ea80e656d1de20
DanGould <d@ngould.dev>
2020-09-07 12:54:53 -0400

    require password to generate address

--- Chaincase/ViewModels/ReceiveViewModel.cs
+++ Chaincase/ViewModels/ReceiveViewModel.cs
@@ -26,19 +26,26 @@ namespace Chaincase.ViewModels
             : base(Locator.Current.GetService<IViewStackService>())
 		{
 			Global = Locator.Current.GetService<Global>();
+
+			var promptViewModel = new PasswordPromptViewModel("ACCEPT", "Generate Address");
+			promptViewModel.ValidatePasswordCommand.Subscribe(async validPassword =>
+			{
+				if (validPassword != null)
+				{
+					await ViewStackService.PopModal();
+					Device.BeginInvokeOnMainThread(() =>
+					{
+						HdPubKey toReceive = Global.Wallet.KeyManager.GetNextReceiveKey(Memo, out bool minGapLimitIncreased);
+						Memo = "";
+						ViewStackService.PushPage(new AddressViewModel(toReceive)).Subscribe();
+					});
+				}
+			});
+
 			GenerateCommand = ReactiveCommand.CreateFromObservable(() =>
 			{
-				Device.BeginInvokeOnMainThread(() =>
-				{
-                    HdPubKey toReceive = Global.Wallet.KeyManager.GetNextReceiveKey(Memo, out bool minGapLimitIncreased);
-                    if (minGapLimitIncreased)
-                    {
-                        int minGapLimit = Global.Wallet.KeyManager.MinGapLimit.Value;
-                        int prevMinGapLimit = minGapLimit - 1;
-                    }
-                    Memo = "";
-					ViewStackService.PushPage(new AddressViewModel(toReceive)).Subscribe();
-				});
+
+				ViewStackService.PushModal(promptViewModel).Subscribe();
 				return Observable.Return(Unit.Default);
 			}, this.WhenAnyValue(vm => vm.Memo, memo => memo.Length > 0));
 		}
