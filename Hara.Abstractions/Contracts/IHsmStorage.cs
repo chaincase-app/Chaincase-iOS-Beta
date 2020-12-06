2846a09eb534641fae7320b42f1d608a136d272f
DanGould <d@ngould.dev>
2020-12-06 13:17:49 -0500

    fix hsm

---
+++ Hara.Abstractions/Contracts/IHsmStorage.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Chaincase.Common
+{
+    // Use keys from device Hardware Security Module to encrypt
+    public interface IHsmStorage
+	{
+        // use the most convenient authentication available in implementation
+        public Task SetAsync(string key, string value);
+
+        public Task<string> GetAsync(string key);
+
+        public bool Remove(string key);
+    }
+}
