3b47665720162a6fe493830f710f2bbf25d14b2c
DanGould <d@ngould.dev>
2020-12-16 14:18:52 -0500

    TransactionList skeleton

--- Chaincase.UI/ViewModels/IndexViewModel.cs
+++ Chaincase.UI/ViewModels/IndexViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Chaincase.Common;
+using Chaincase.Common.Models;
 using ReactiveUI;
 using Splat;
 using WalletWasabi.Blockchain.Keys;
+using WalletWasabi.Blockchain.Transactions;
 using WalletWasabi.Logging;
+using WalletWasabi.Models;
 using WalletWasabi.Wallets;
 using Xamarin.Forms;
 
@@ -18,6 +23,7 @@ namespace Chaincase.UI.ViewModels
         protected Global Global { get; }
 
         private CompositeDisposable Disposables { get; set; }
+        private ObservableCollection<TransactionViewModel> _transactions;
         public string _balance;
         private ObservableAsPropertyHelper<bool> _hasCoins;
         private ObservableAsPropertyHelper<bool> _hasSeed;
@@ -30,11 +36,19 @@ namespace Chaincase.UI.ViewModels
         {
             Global = Locator.Current.GetService<Global>();
             if (Global.WalletExists())
+			{
                 Global.SetDefaultWallet();
                 Task.Run(async () => await LoadWalletAsync());
 
-            // init with UI config
+                Transactions = new ObservableCollection<TransactionViewModel>();
+                TryWriteTableFromCache();
+            }
+                
+
             Balance = Global.UiConfig.Balance;
+
+            
+
 
             Initializing += OnInit;
             Initializing(this, EventArgs.Empty);
@@ -47,7 +61,7 @@ namespace Chaincase.UI.ViewModels
         {
             Initializing -= OnInit;
 
-            while (Global.Wallet.State < WalletState.Initialized)
+            while (Global.Wallet == null || Global.Wallet.State < WalletState.Initialized)
             {
                 await Task.Delay(200);
             }
@@ -64,7 +78,57 @@ namespace Chaincase.UI.ViewModels
                        Balance = Global.Wallet.Coins.TotalAmount().ToString();
                    });
 
+                Observable.FromEventPattern(Global.Wallet, nameof(Global.Wallet.NewBlockProcessed))
+                    .Merge(Observable.FromEventPattern(Global.Wallet.TransactionProcessor, nameof(Global.Wallet.TransactionProcessor.WalletRelevantTransactionProcessed)))
+                    .Throttle(TimeSpan.FromSeconds(3))
+                    .ObserveOn(RxApp.MainThreadScheduler)
+                    .Subscribe(async _ => await TryRewriteTableAsync());
+
             });
+        }
+
+        private void TryWriteTableFromCache()
+        {
+            try
+            {
+                var trs = Global.UiConfig.Transactions.Select(ti => new TransactionViewModel(ti));
+                Transactions = new ObservableCollection<TransactionViewModel>(trs.OrderByDescending(t => t.DateTime));
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+            }
+        }
+
+        private async Task TryRewriteTableAsync()
+        {
+            try
+            {
+                var historyBuilder = new TransactionHistoryBuilder(Global.Wallet);
+                var txRecordList = await Task.Run(historyBuilder.BuildHistorySummary);
+                var tis = txRecordList.Select(txr => new TransactionInfo
+                {
+                    DateTime = txr.DateTime.ToLocalTime(),
+                    Confirmed = txr.Height.Type == HeightType.Chain,
+                    Confirmations = txr.Height.Type == HeightType.Chain ? (int)Global.BitcoinStore.SmartHeaderChain.TipHeight - txr.Height.Value + 1 : 0,
+                    AmountBtc = $"{txr.Amount.ToString(fplus: true, trimExcessZero: true)}",
+                    Label = txr.Label,
+                    BlockHeight = txr.Height.Type == HeightType.Chain ? txr.Height.Value : 0,
+                    TransactionId = txr.TransactionId.ToString()
+                });
+
+                Transactions?.Clear();
+                var trs = tis.Select(ti => new TransactionViewModel(ti));
+
+                Transactions = new ObservableCollection<TransactionViewModel>(trs.OrderByDescending(t => t.DateTime));
+
+                Global.UiConfig.Transactions = tis.ToArray();
+                Global.UiConfig.ToFile(); // write to file once height is the highest
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+            }
         }
 
         private static async Task LoadWalletAsync()
@@ -96,6 +160,12 @@ namespace Chaincase.UI.ViewModels
         {
             get => _balance;
             set => this.RaiseAndSetIfChanged(ref _balance, value);
+        }
+
+        public ObservableCollection<TransactionViewModel> Transactions
+        {
+            get => _transactions;
+            set => this.RaiseAndSetIfChanged(ref _transactions, value);
         }
     }
 }
