01d5819cb980837ea03569c97a46d19acc6280b6
Armin Sabouri <armin@team.casa>
2020-12-30 09:53:01 -0500

    Revert testnet uri in config.cs

--- Chaincase.Common/Config.cs
+++ Chaincase.Common/Config.cs
@@ -32,13 +32,13 @@ namespace Chaincase.Common
 
         [JsonProperty(PropertyName = "Network")]
         [JsonConverter(typeof(NetworkJsonConverter))]
-        public NBitcoin.Network Network { get; internal set; } = NBitcoin.Network.TestNet;
+        public NBitcoin.Network Network { get; internal set; } = NBitcoin.Network.Main;
 
         [DefaultValue("http://cmekpfcgcdmaegqdsj4x4j6qkdem2jhndnboegwhf3jwr2mubafjl3id.onion")]
         [JsonProperty(PropertyName = "MainNetBackendUriV3", DefaultValueHandling = DefaultValueHandling.Populate)]
         public string MainNetBackendUriV3 { get; private set; }
 
-        [DefaultValue("http://tv4a3m3ptwrt5si4b72ufpj7uashnyz3yelo4szchgdkhnh6eoygvyqd.onion")]
+        [DefaultValue("http://testwnp3fugjln6vh5vpj7mvq3lkqqwjj3c2aafyu7laxz42kgwh2rad.onion")]
         [JsonProperty(PropertyName = "TestNetBackendUriV3", DefaultValueHandling = DefaultValueHandling.Populate)]
         public string TestNetBackendUriV3 { get; private set; }
 
