c64c4b6c4674c1b023ac220e1d3810fb89a28cf4
DanGould <d@ngould.dev>
2021-01-05 22:32:41 -0500

    disable gen address button on label empty

--- Chaincase.UI/Services/Extensions.cs
+++ Chaincase.UI/Services/Extensions.cs
@@ -32,5 +32,20 @@ namespace Chaincase.UI.Services
 		{
 			navigationManager.NavigateTo(route);
 		}
+
+		public static bool IsNullOrWhiteSpace(this string value)
+		{
+			if (value != null)
+			{
+				for (int i = 0; i < value.Length; i++)
+				{
+					if (!char.IsWhiteSpace(value[i]))
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
 	}
 }
