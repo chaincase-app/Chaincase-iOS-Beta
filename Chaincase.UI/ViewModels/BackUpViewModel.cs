892aa6da70a1f57778aecfe213a50ae1cbede043
DanGould <d@ngould.dev>
2021-01-12 23:31:19 -0500

    hook up BackUpViewModel to fetch hsm seed words

---
+++ Chaincase.UI/ViewModels/BackUpViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Chaincase.Common;
+using Chaincase.Common.Contracts;
+using ReactiveUI;
+using Splat;
+
+namespace Chaincase.UI.ViewModels
+{
+    public class BackUpViewModel : ReactiveObject
+    {
+        protected Global Global { get; }
+        protected IHsmStorage HSM { get; }
+
+        private List<string> _seedWords;
+
+        public BackUpViewModel(Global global, IHsmStorage hsm)
+        {
+            Global = global;
+            HSM = hsm;
+        }
+
+        public async Task<bool> HasGotSeedWords()
+        {
+            var seedWords = await HSM.GetAsync($"{Global.Network}-seedWords");
+            if (seedWords is null) return false;
+
+            SeedWords = seedWords.Split(' ').ToList();
+            return true;
+        }
+
+        public List<string> SeedWords
+        {
+            get => _seedWords;
+            set => this.RaiseAndSetIfChanged(ref _seedWords, value);
+        }
+    }
+}
