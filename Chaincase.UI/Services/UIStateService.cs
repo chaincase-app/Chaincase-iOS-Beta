2f59ff927e61edcae84f2f797c3b36431cd01a5a
DanGould <d@ngould.dev>
2021-01-25 11:39:46 -0500

    create apperance page to select themes

--- Chaincase.UI/Services/UIStateService.cs
+++ Chaincase.UI/Services/UIStateService.cs
@@ -4,9 +4,20 @@ using Chaincase.Common.Contracts;
 
 namespace Chaincase.UI.Services
 {
+	public enum Theme
+	{
+		System,
+		Light,
+		Dark
+	}
+
 	public class UIStateService
 	{
+
+
+
 		private string _title;
+		private Theme _theme;
 		private bool _darkMode;
 		protected IThemeManager _themeManager;
 
@@ -28,9 +39,31 @@ namespace Chaincase.UI.Services
 			}
 		}
 
+		public Theme Theme
+		{
+			get => _theme;
+			set
+			{
+				_theme = value;
+				SetSystemTheme();
+			}
+		}
+
 		public void SetSystemTheme()
 		{
-			DarkMode = _themeManager.IsDarkTheme();
+			switch(Theme)
+			{
+				case Theme.Light:
+					DarkMode = false;
+					break;
+				case Theme.Dark:
+					DarkMode = true;
+					break;
+				case Theme.System:
+				default:
+					DarkMode = _themeManager.IsDarkTheme();
+					break;
+			}
 		}
 
 		public bool DarkMode
