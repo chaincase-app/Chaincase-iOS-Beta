87d4e8a189fe37fe9e048cca006c5693083857e8
DanGould <d@ngould.dev>
2021-03-27 12:33:35 -0400

    flesh out common services

--- Chaincase.Common/Extensions.cs
+++ Chaincase.Common/Extensions.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.IO;
+using Chaincase.Common.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using WalletWasabi.Blockchain.Blocks;
+using WalletWasabi.Blockchain.Mempool;
+using WalletWasabi.Blockchain.Transactions;
+using WalletWasabi.Stores;
+using WalletWasabi.Wallets;
 
 namespace Chaincase.Common
 {
@@ -9,6 +16,17 @@ namespace Chaincase.Common
             services.AddSingleton<Global>();
             services.AddSingleton<Config>();
             services.AddSingleton<UiConfig>();
+            services.AddSingleton<WalletManager>();
+            services.AddSingleton(x =>
+            {
+                var config = x.GetRequiredService<Config>();
+                var dataDir = x.GetRequiredService<IDataDirProvider>().Get();
+                var indexStore = new IndexStore(config.Network, new SmartHeaderChain());
+
+                return new BitcoinStore(Path.Combine(dataDir, "BitcoinStore"), config.Network,
+                    indexStore, new AllTransactionStore(), new MempoolService()
+                );
+            });
 
             return services;
         }
