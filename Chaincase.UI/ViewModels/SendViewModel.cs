c68c810c38540459c28e520058c769d51d82f404
DanGould <d@ngould.dev>
2021-04-03 17:29:12 -0400

    trim destination on parse

--- Chaincase.UI/ViewModels/SendViewModel.cs
+++ Chaincase.UI/ViewModels/SendViewModel.cs
@@ -133,6 +133,7 @@ namespace Chaincase.UI.ViewModels
         internal BitcoinUrlBuilder ParseDestinationString(string destinationString)
 		{
             if (destinationString == null) return null;
+            destinationString = destinationString.Trim();
             BitcoinUrlBuilder url = null;
             try
             {
