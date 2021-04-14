c88ca0d7e55f5a62def9d35f47b13a2e95a0392c
DanGould <d@ngould.dev>
2021-04-14 11:56:25 -0400

    clean Chaincase proj

--- Chaincase/BlazorApp.cs
+++ Chaincase/BlazorApp.cs
@@ -5,6 +5,7 @@ using Chaincase.Common;
 using Chaincase.Common.Contracts;
 using Chaincase.Services;
 using Chaincase.UI.Services;
+using Chaincase.UI.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
@@ -14,94 +15,93 @@ using Xamarin.Forms;
 
 namespace Chaincase
 {
-	public class BlazorApp : Application
-	{
-		private readonly IHost _host;
+    public class BlazorApp : Application
+    {
+        private readonly IHost _host;
 
-		public IServiceProvider ServiceProvider => _host.Services;
+        public IServiceProvider ServiceProvider => _host.Services;
 
-		public BlazorApp(IFileProvider fileProvider = null, Action<IServiceCollection> configureDI = null)
-		{
-			var hostBuilder = MobileBlazorBindingsHost.CreateDefaultBuilder()
-				.ConfigureServices((hostContext, services) =>
-				{
-					services.AddCommonServices();
-					services.AddUIServices();
+        private event EventHandler Resuming = delegate { };
 
-					// Adds web-specific services such as NavigationManager
-					services.AddBlazorHybrid();
+        public BlazorApp(IFileProvider fileProvider = null, Action<IServiceCollection> configureDI = null)
+        {
+            var hostBuilder = MobileBlazorBindingsHost.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddCommonServices();
+                    services.AddUIServices();
 
-					services.AddScoped<IClipboard, XamarinClipboard>();
-					services.AddSingleton<IDataDirProvider, XamarinDataDirProvider>();
-					services.AddSingleton<IMainThreadInvoker, XamarinMainThreadInvoker>();
-					services.AddSingleton<IShare, XamarinShare>();
-					services.AddSingleton<IThemeManager, XamarinThemeManager>();
+                    // Adds web-specific services such as NavigationManager
+                    services.AddBlazorHybrid();
 
-					configureDI?.Invoke(services);
-				})
-				.UseWebRoot("wwwroot");
+                    services.AddScoped<IClipboard, XamarinClipboard>();
+                    services.AddSingleton<IDataDirProvider, XamarinDataDirProvider>();
+                    services.AddSingleton<IMainThreadInvoker, XamarinMainThreadInvoker>();
+                    services.AddSingleton<IShare, XamarinShare>();
+                    services.AddSingleton<IThemeManager, XamarinThemeManager>();
 
-			if (fileProvider != null)
-			{
-				hostBuilder.UseStaticFiles(fileProvider);
-			}
-			else
-			{
-				hostBuilder.UseStaticFiles();
-			}
+                    configureDI?.Invoke(services);
+                })
+                .UseWebRoot("wwwroot");
 
-			_host = hostBuilder.Build();
+            if (fileProvider != null)
+            {
+                hostBuilder.UseStaticFiles(fileProvider);
+            }
+            else
+            {
+                hostBuilder.UseStaticFiles();
+            }
+
+            _host = hostBuilder.Build();
 
 
-			MainPage = new ContentPage { Title = "Chaincase" };
+            MainPage = new ContentPage { Title = "Chaincase" };
 
-			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-			TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
 
-			_host.AddComponent<Main>(parent: MainPage);
-		}
+            _host.AddComponent<Main>(parent: MainPage);
+        }
 
-		public void InitializeNoWallet()
-		{
-			// This relies on Global registered
-			var message = new InitializeNoWalletTaskMessage();
-			MessagingCenter.Send(message, "InitializeNoWalletTaskMessage");
-		}
+        public void InitializeNoWallet()
+        {
+            var message = new InitializeNoWalletTaskMessage();
+            MessagingCenter.Send(message, "InitializeNoWalletTaskMessage");
+        }
 
-		protected override void OnSleep()
-		{
-			// Execute Sleeping Background Task
-			var message = new OnSleepingTaskMessage();
-			MessagingCenter.Send(message, "OnSleepingTaskMessage");
-		}
+        protected override void OnSleep()
+        {
+            // Execute Sleeping Background Task
+            var message = new OnSleepingTaskMessage();
+            MessagingCenter.Send(message, "OnSleepingTaskMessage");
+        }
 
-		protected override void OnResume()
-		{
-			Resuming += OnResuming;
-			// Execute Async code
-			Resuming(this, EventArgs.Empty);
-		}
+        protected override void OnResume()
+        {
+            Resuming += OnResuming;
+            // Execute Async code
+            Resuming(this, EventArgs.Empty);
+        }
 
-		private event EventHandler Resuming = delegate { };
+        private async void OnResuming(object sender, EventArgs args)
+        {
+            //unsubscribe from event
+            Resuming -= OnResuming;
 
-		private async void OnResuming(object sender, EventArgs args)
-		{
-			//unsubscribe from event
-			Resuming -= OnResuming;
+            //perform non-blocking actions
+            await ServiceProvider.GetRequiredService<Global>().OnResuming();
+        }
 
-			//perform non-blocking actions
-			await ServiceProvider.GetRequiredService<Global>().OnResuming();
-		}
+        private static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Logger.LogWarning(e?.Exception, "UnobservedTaskException");
+        }
 
-		private static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
-		{
-			Logger.LogWarning(e?.Exception, "UnobservedTaskException");
-		}
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Logger.LogWarning(e?.ExceptionObject as Exception, "UnhandledException");
+        }
 
-		private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-		{
-			Logger.LogWarning(e?.ExceptionObject as Exception, "UnhandledException");
-		}
-
-	}
+    }
 }
