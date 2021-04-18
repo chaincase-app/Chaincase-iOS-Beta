9202c08e5ded885f8c3a9258e0c00452c57e1803
DanGould <d@ngould.dev>
2021-04-18 19:58:51 -0400

    spacing

--- Chaincase.UI/Services/UIStateService.cs
+++ Chaincase.UI/Services/UIStateService.cs
@@ -4,53 +4,53 @@ using Chaincase.Common.Contracts;
 
 namespace Chaincase.UI.Services
 {
-	public class UIStateService
-	{
-		private string _title;
-		private bool _darkMode;
-		private readonly Global _global;
-		private readonly IThemeManager _themeManager;
+    public class UIStateService
+    {
+        private string _title;
+        private bool _darkMode;
+        private readonly Global _global;
+        private readonly IThemeManager _themeManager;
 
-		public UIStateService(Global global, IThemeManager themeManager)
-		{
-			_global = global;
-			_global.Resumed += (s, e) => SetSystemTheme();
-			_themeManager = themeManager;
-			themeManager.SubscribeToThemeChanged(() => SetSystemTheme());
-		}
+        public UIStateService(Global global, IThemeManager themeManager)
+        {
+            _global = global;
+            _global.Resumed += (s, e) => SetSystemTheme();
+            _themeManager = themeManager;
+            themeManager.SubscribeToThemeChanged(() => SetSystemTheme());
+        }
 
-		public void OnResume(object sender, EventArgs args)
-		{
-			SetSystemTheme();
-		}
+        public void OnResume(object sender, EventArgs args)
+        {
+            SetSystemTheme();
+        }
 
-		public string Title
-		{
-			get => _title;
-			set
-			{
-				_title = value;
-				StateChanged?.Invoke();
-			}
-		}
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                StateChanged?.Invoke();
+            }
+        }
 
-		public void SetSystemTheme()
-		{
-			DarkMode = _themeManager.IsDarkTheme();
-		}
+        public void SetSystemTheme()
+        {
+            DarkMode = _themeManager.IsDarkTheme();
+        }
 
-		public bool DarkMode
-		{
-			get => _darkMode;
-			set
-			{
-				_darkMode = value;
-				StateChanged?.Invoke();
-				ThemeChanged?.Invoke();
-			}
-		}
+        public bool DarkMode
+        {
+            get => _darkMode;
+            set
+            {
+                _darkMode = value;
+                StateChanged?.Invoke();
+                ThemeChanged?.Invoke();
+            }
+        }
 
-		public event Action StateChanged;
-		public event Action ThemeChanged;
-	}
+        public event Action StateChanged;
+        public event Action ThemeChanged;
+    }
 }
