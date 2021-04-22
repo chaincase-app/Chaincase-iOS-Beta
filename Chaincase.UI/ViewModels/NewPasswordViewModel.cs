ee31faefbaad73bc0bc1d62f24e53ccf7fcf9076
DanGould <d@ngould.dev>
2021-04-22 16:10:12 -0400

    use sensitiveStorage @ new wallet

--- Chaincase.UI/ViewModels/NewPasswordViewModel.cs
+++ Chaincase.UI/ViewModels/NewPasswordViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
+using System.Threading.Tasks;
 using Chaincase.Common;
 using Chaincase.Common.Contracts;
+using Chaincase.Common.Services;
 using NBitcoin;
 using ReactiveUI;
 using WalletWasabi.Blockchain.Keys;
@@ -9,35 +11,36 @@ using WalletWasabi.Wallets;
 
 namespace Chaincase.UI.ViewModels
 {
-	public class NewPasswordViewModel : ReactiveObject
-	{
-		private readonly WalletManager _walletManager;
-		private readonly Config _config;
-		private readonly UiConfig _uiConfig;
-		private readonly IHsmStorage _hsm;
+    public class NewPasswordViewModel : ReactiveObject
+    {
+        private readonly WalletManager _walletManager;
+        private readonly Config _config;
+        private readonly UiConfig _uiConfig;
+        private readonly IHsmStorage _hsm;
 
-		public NewPasswordViewModel(WalletManager walletManager, Config config, UiConfig uiConfig, IHsmStorage hsmStorage)
-		{
-			_walletManager = walletManager;
-			_config = config;
-			_uiConfig = uiConfig;
-			_hsm = hsmStorage;
-		}
+        public NewPasswordViewModel(WalletManager walletManager, Config config, UiConfig uiConfig, IHsmStorage hsmStorage)
+        {
+            _walletManager = walletManager;
+            _config = config;
+            _uiConfig = uiConfig;
+            _hsm = hsmStorage;
+        }
 
-		public void SetPassword(string password)
-		{
-			// Here we are not letting anything that will be autocorrected later.
-			// Generate wallet with password exactly as entered for compatibility.
-			// Todo what do we do if PasswordHelper.Guard fails?
-			PasswordHelper.Guard(password);
+        public async Task SetPasswordAsync(string password)
+        {
+            // Here we are not letting anything that will be autocorrected later.
+            // Generate wallet with password exactly as entered for compatibility.
+            PasswordHelper.Guard(password);
 
-			string walletFilePath = Path.Combine(_walletManager.WalletDirectories.WalletsDir, $"{_config.Network}.json");
-			KeyManager.CreateNew(out Mnemonic seedWords, password, walletFilePath);
-			// MUST prompt permissions
-			_hsm.SetAsync($"{_config.Network}-seedWords", seedWords.ToString());
+            string walletFilePath = Path.Combine(_walletManager.WalletDirectories.WalletsDir, $"{_config.Network}.json");
+            KeyManager.CreateNew(out Mnemonic seedWords, password, walletFilePath);
 
-			_uiConfig.HasSeed = true;
-			_uiConfig.ToFile();
-		}
-	}
+            var cryptor = new SensitiveStorage(_hsm, _config.Network);
+            await cryptor.SetSeedWords(password, seedWords.ToString());
+
+            // this should not be a config
+            _uiConfig.HasSeed = true;
+            _uiConfig.ToFile();
+        }
+    }
 }
