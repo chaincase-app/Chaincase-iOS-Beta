271b1af23061fd263a783fa090d116a8ce9482d3
DanGould <d@ngould.dev>
2021-04-28 17:14:53 -0400

    fix test config setup

--- Chaincase.Tests/StorageTests.cs
+++ Chaincase.Tests/StorageTests.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Chaincase.Common;
 using Chaincase.Common.Contracts;
 using Chaincase.Common.Services;
 using NBitcoin;
+using WalletWasabi.Helpers;
 using Xunit;
 
 namespace Chaincase.Tests
@@ -14,8 +16,11 @@ namespace Chaincase.Tests
         [Fact]
         public void CanStoreSeedWords()
         {
-            var mockConfig = new Config("");
-            SensitiveStorage storage = new(new MockHsm(), mockConfig);
+            var testDir = EnvironmentHelpers.GetDataDir(Path.Combine("Chaincase", "Tests", "StorageTests"));
+            var config = new Config(Path.Combine(testDir, "Config.json"));
+            var uiConfig = new UiConfig(Path.Combine(testDir, "UiConfig.json"));
+
+            SensitiveStorage storage = new(new MockHsm(), config, uiConfig);
             string password = "password";
             Mnemonic mnemonic = new(Wordlist.English);
             storage.SetSeedWords(password, mnemonic.ToString());
