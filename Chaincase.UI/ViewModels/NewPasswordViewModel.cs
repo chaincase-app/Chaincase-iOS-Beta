chaincase-app/chaincase

Switch to IOptions pattern

Chaincase.UI/ViewModels/NewPasswordViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Chaincase.Common;
 using Chaincase.Common.Contracts;
+using Microsoft.Extensions.Options;
 using NBitcoin;
 using ReactiveUI;
 using WalletWasabi.Blockchain.Keys;
@@ -12,11 +13,11 @@ namespace Chaincase.UI.ViewModels
 	public class NewPasswordViewModel : ReactiveObject
 	{
 		private readonly WalletManager _walletManager;
-		private readonly Config _config;
-		private readonly UiConfig _uiConfig;
+		private readonly IOptions<Config> _config;
+		private readonly IOptions<UiConfig> _uiConfig;
 		private readonly IHsmStorage _hsm;
 
-		public NewPasswordViewModel(WalletManager walletManager, Config config, UiConfig uiConfig, IHsmStorage hsmStorage)
+		public NewPasswordViewModel(WalletManager walletManager, IOptions<Config> config, IOptions<UiConfig> uiConfig, IHsmStorage hsmStorage)
 		{
 			_walletManager = walletManager;
 			_config = config;
@@ -31,13 +32,13 @@ namespace Chaincase.UI.ViewModels
 			// Todo what do we do if PasswordHelper.Guard fails?
 			PasswordHelper.Guard(password);
 
-			string walletFilePath = Path.Combine(_walletManager.WalletDirectories.WalletsDir, $"{_config.Network}.json");
+			string walletFilePath = Path.Combine(_walletManager.WalletDirectories.WalletsDir, $"{_config.Value.Network}.json");
 			KeyManager.CreateNew(out Mnemonic seedWords, password, walletFilePath);
 			// MUST prompt permissions
-			_hsm.SetAsync($"{_config.Network}-seedWords", seedWords.ToString());
+			_hsm.SetAsync($"{_config.Value.Network}-seedWords", seedWords.ToString());
 
-			_uiConfig.HasSeed = true;
-			_uiConfig.ToFile();
+			_uiConfig.Value.HasSeed = true;
+			_uiConfig.Value.ToFile();
 		}
 	}
 }

chaincase-app/Chaincase-iOS-Beta

Chaincase.UI/ViewModels/NewPasswordViewModel.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Chaincase.Common;
 using Chaincase.Common.Services;
 using Chaincase.Common.Contracts;
+using Microsoft.Extensions.Options;
 using NBitcoin;
 using ReactiveUI;
 using WalletWasabi.Blockchain.Keys;
@@ -15,8 +16,8 @@ namespace Chaincase.UI.ViewModels
     public class NewPasswordViewModel : ReactiveObject
     {
         private readonly WalletManager _walletManager;
-        private readonly Config _config;
-        private readonly UiConfig _uiConfig;
+        private readonly IOptions<Config> _config;
+        private readonly IOptions<UiConfig> _uiConfig;
         private readonly SensitiveStorage _storage;
 
         public const int PasswordMinLength = 8;
@@ -25,7 +26,7 @@ namespace Chaincase.UI.ViewModels
         [MinLength(PasswordMinLength, ErrorMessage = "Make it 8 or more characters")]
         public string Password { get; set; }
 
-        public NewPasswordViewModel(ChaincaseWalletManager walletManager, Config config, UiConfig uiConfig, SensitiveStorage storage)
+        public NewPasswordViewModel(ChaincaseWalletManager walletManager, IOptions<Config> config, IOptions<UiConfig> uiConfig, SensitiveStorage storage)
         {
             _walletManager = walletManager;
             _config = config;