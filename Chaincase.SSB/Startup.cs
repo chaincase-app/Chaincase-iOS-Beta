284c3373d00fb6a572882f29344020bb829afa1b
Kukks <evilkukka@gmail.com>
2021-11-29 14:54:24 +0100

    make web camera work too

--- Chaincase.SSB/Startup.cs
+++ Chaincase.SSB/Startup.cs
@@ -39,7 +39,7 @@ namespace Chaincase.SSB
 			services.AddSingleton<IMainThreadInvoker, SSBMainThreadInvoker>();
 			services.AddScoped<IShare, SSBShare>();
 			services.AddSingleton<IThemeManager, MockThemeManager>();
-			services.AddSingleton<ICameraScanner, MockCameraScanner>();
+			services.AddSingleton<ICameraScanner, WebCameraScanner>();
 
 			// typically OS specific but web specific in SSB
 			services.AddScoped<IHsmStorage, JsInteropSecureConfigProvider>();
---
+++ Chaincase.SSB/WebCameraScanner.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using BlazorBarcodeScanner.ZXing.JS;
+using Chaincase.Common.Contracts;
+
+namespace Chaincase.SSB
+{
+	public class WebCameraScanner : ICameraScanner
+	{
+		private ZXingBlazorCameraScanner _reader;
+		private TaskCompletionSource<string> tcs;
+
+		public void SetRef(ZXingBlazorCameraScanner barcodeReader)
+		{
+			_reader = barcodeReader;
+		}
+
+		public Task<string> Scan()
+		{
+			tcs = new TaskCompletionSource<string>();
+			_reader.ShowNow();
+			return tcs.Task;
+		}
+
+		public void OnBarcodeReceived(BarcodeReceivedEventArgs barcodeReceivedEventArgs)
+		{
+			tcs?.SetResult(barcodeReceivedEventArgs.BarcodeText);
+		}
+	}
+}
